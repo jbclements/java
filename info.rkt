0ac946beef7b537a379d6a741b23db8c76b6f26e
John Clements <clements@racket-lang.org>
2014-08-26 09:46:28 -0700

    added dependencies

--- info.rkt
+++ info.rkt
@@ -1,4 +1,10 @@
 #lang setup/infotab
 (define collection 'multi)
-(define deps (list "dherman-struct" "io"))
+(define deps (list "dherman-struct"
+                   "io"
+                   "base"
+                   "compatibility-lib"
+                   "parser-tools-lib"
+                   "srfi-lite-lib"))
 
+(define build-deps (list "rackunit-lib"))
