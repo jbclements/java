b7c02aeb957e8320f17bd8ca42861c23fdcf754f
John Clements <clements@racket-lang.org>
2014-11-06 11:04:15 -0800

    added scribblings field

--- java/info.rkt
+++ java/info.rkt
@@ -3,5 +3,6 @@
 (define name "Java Metaprogramming Tools")
 (define blurb
   (list "A library for manipulating Java programs in both source and binary format."))
+(define scribblings '(("java.scrbl" () (parsing-library))))
 (define categories '(metaprogramming devtools))
 (define version "0.1")
